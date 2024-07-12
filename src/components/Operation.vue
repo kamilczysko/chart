68907ad68803407d35f8eaef17a9d0f82c6f88a4
Kamil <kjas92@gmail.com>
2024-07-12 23:22:18 +0200

    fix dragging

--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -40,27 +40,73 @@ export default {
         this.posX = this.x;
     },
     methods: {
+    
+        onMouseMove(event) {
+            if (this.dragging) {
+                this.posX = event.clientX - this.offsetX;
+            }
+        },
+        onMouseUp() {
+            this.dragging = false;
+
+            const offset = this.posX - this.operation.startPosition;
+            this.$store.commit("moveSelected", offset);
+
+            this.$emit("dragEnd")
+
+            document.removeEventListener('mousemove', this.onMouseMove);
+            document.removeEventListener('mouseup', this.onMouseUp);
+        },
+        // onMouseDownRightResize(event) {
+        //     this.draggResize = true;
+        //     this.posXBeforeResize = event.clientX;       
+        //     this.widthBeforeResize = this.width;
+
+        //     document.addEventListener('mousemove', this.onMouseMoveResize);
+        //     document.addEventListener('mouseup', this.onMouseUpResize);
+
+        //     this.$store.commit("selectOperation", this.operation.id);
+        // },
+        // onMouseMoveResize(event) {
+        //     if (this.draggResize) {
+        //         this.width = this.width + (event.clientX - this.posXBeforeResize);
+        //         this.posXBeforeResize = event.clientX;
+        //     }
+        // },
+        // onMouseUpResize() {
+        //     this.draggResize = false;
+
+        //     const offset = this.width - this.widthBeforeResize;
+        //     this.$store.commit("resizeSelected", {startPosition: this.posX, offset: offset});
+            
+        //     document.removeEventListener('mousemove', this.onMouseMove);
+        //     document.removeEventListener('mouseup', this.onMouseUpResize);
+        // },
         dragStart(event) {
             this.dragging = true;
-            this.dragStartX = event.clientX;
+            this.offsetX = event.clientX - this.posX;
+            
+            document.addEventListener('mousemove', this.move);
+            document.addEventListener('mouseup', this.stopDrag);
+
             this.$store.commit("selectOperation", this.id);
             
         },
         move(event) {
-            if(this.dragging) {
-                this.offsetX = event.clientX - this.dragStartX;
-                this.dragStartX = event.clientX;
-                console.log(this.dragStartX)
-                this.posX = event.clientX;
-
+            if (this.dragging) {
+                this.posX = event.clientX - this.offsetX;
             }
         },
         dragStop(event) {
-            if(this.dragging) {
+            if(this.dragging){
                 this.dragging = false;
-                console.log("sex")
-                this.$store.commit('addOffsetToOperation', {id:this.id, offset: this.offsetX});
+                const offset = this.posX - this.operation.startPosition-this.labelsWidth;
+
+                this.$store.commit('addOffsetToOperation', {id: this.id, offset: offset});
                 this.posX = this.operation.startPosition + this.labelsWidth;
+                
+                document.removeEventListener('mousemove', this.move);
+                document.removeEventListener('mouseup', this.drag);
             }
         }
     }
